f0d639e873b0dcbc6ecc6461c483432e5190476d
oliverbskau <oliverbskau@gmail.com>
2022-03-27 21:18:49 +0200

    added actual items to electronics page

--- src/pages/Ëlectronics/Electronics.svelte
+++ src/pages/Ëlectronics/Electronics.svelte
@@ -1,11 +1,18 @@
 <script>
 
+let elecpic1 = "./images/elecpic1.jpg";
+let elecpic2 = "./images/elecpic2.webp";
+let elecpic3 = "./images/elecpic3.jpg";
+let elecpic4 = "./images/elecpic4.webp";
+let elecpic5 = "./images/elecpic5.webp";
+
+
 let electronicalItems = [
-    {image: "testImage1", name: "testName", price: "testPrice", description: "testDescription" },
-    {image: "testImage2", name: "testName", price: "testPrice", description: "testDescription" },
-    {image: "testImage3", name: "testName", price: "testPrice", description: "testDescription" },
-    {image: "testImage4", name: "testName", price: "testPrice", description: "testDescription" },
-    {image: "testImage5", name: "testName", price: "testPrice", description: "testDescription" }
+    {image: elecpic1, name: "Kea Super Computer v3", price: "$1000", description: "Testwinning computer for larger coding projects" },
+    {image: elecpic2, name: "Stretchy-lab-top", price: "$560", description: "The new stretchy labtop from kea-ware" },
+    {image: elecpic3, name: "PS5 controller", price: "$103", description: "The best and most immersive gaming controller" },
+    {image: elecpic4, name: "Nintendo Switch", price: "$299", description: "The best portable family gaming console" },
+    {image: elecpic5, name: "Flippedy-flop-phone", price: "$900", description: "Why not bottle-flip the new flippy-phone? Always lands perfect" }
 ]
 
 </script>
@@ -28,7 +35,9 @@ let electronicalItems = [
         </tr>
         {#each electronicalItems as electronicalItem}
         <tr>
-            <td>{electronicalItem.image}</td>
+            <td>
+                <img class="tableImg" src={electronicalItem.image} alt="electronicalItem"/>
+            </td>
             <td>{electronicalItem.name}</td>
             <td>{electronicalItem.price}</td>
             <td>{electronicalItem.description}</td>
